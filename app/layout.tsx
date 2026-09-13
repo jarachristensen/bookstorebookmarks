bb684c401d07d00640951a1b86a0fd5d96701340
Jara Christensen <jarachristensen@Jaras-MacBook-Air.local>
2026-09-12 22:17:10 -0500

    fix(analytics): import from @vercel/analytics/next

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import React from "react";
-import { Analytics } from "@vercel/analytics/react";
+import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
